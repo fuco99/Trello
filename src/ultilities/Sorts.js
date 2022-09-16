aa638558ced30318a931dfe705c5c83c8164dc49
Phannt <phannguyentien1999@gmail.com>
2022-09-16 14:41:09 +0700

    Initial sample data

---
+++ src/ultilities/Sorts.js
@@ -0,0 +1,6 @@
+// order an array of objects base on another array order
+
+export const mapOrder = (array, order, key) => {
+  array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
+  return array;
+};
