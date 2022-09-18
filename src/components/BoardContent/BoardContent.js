9d30fe3ee0bf94a94fdd455f859f0f9619304765
Phannt <phannguyentien1999@gmail.com>
2022-09-18 23:08:33 +0700

    Add feature create new column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Container, Draggable } from "react-smooth-dnd";
 import Column from "../Column/Column";
 import { isEmpty } from "lodash";
@@ -6,9 +6,23 @@ import { initialData } from "actions/initialData";
 import { mapOrder } from "ultilities/Sorts";
 import { applyDrag } from "ultilities/dragDrop";
 import "./BoardContent.scss";
+import {
+  Row,
+  Col,
+  Container as BootContainer,
+  Form,
+  Button,
+} from "react-bootstrap";
 function BoardContent() {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [newColumnTitle, setNewColumnTitle] = useState("");
+  const [openNewColumn, setOpenNewColumn] = useState(false);
+  const newColumnInputRef = useRef(null);
+  const onNewColumnTitleChange = useCallback(
+    (e) => setNewColumnTitle(e.target.value),
+    []
+  );
   useEffect(() => {
     const boardFromDb = initialData.boards.find(
       (board) => board.id === "board-1"
@@ -18,6 +32,13 @@ function BoardContent() {
       setColumns(mapOrder(boardFromDb.columns, boardFromDb.columnOrder, "id"));
     }
   }, []);
+
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+      newColumnInputRef.current.select();
+    }
+  }, [openNewColumn]);
 
   if (isEmpty(board)) {
     return (
@@ -44,6 +65,30 @@ function BoardContent() {
       setColumns(newColumns);
     }
   };
+  const toggleNewColumn = () => setOpenNewColumn(!openNewColumn);
+
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      newColumnInputRef.current.focus();
+      return;
+    }
+    const newColumnToAdd = {
+      id: Math.random().toString(36).substr(2, 5),
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: [],
+    };
+    let newColumns = [...columns];
+    newColumns.push(newColumnToAdd);
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((column) => column.id);
+    newBoard.columns = newColumns;
+    setColumns(newColumns);
+    setBoard(newBoard);
+    setNewColumnTitle("");
+    toggleNewColumn();
+  };
   return (
     <div className="board-content" style={{ display: "flex" }}>
       <Container
@@ -65,10 +110,36 @@ function BoardContent() {
           </Draggable>
         ))}
       </Container>
-      <div className="add-new-column">
-        <i className="fa fa-plus" style={{ paddingRight: "3px" }} />
-        Add another list
-      </div>
+      <BootContainer className="trello-container-bootstrap">
+        {!openNewColumn && (
+          <Row>
+            <Col className="add-new-column" onClick={toggleNewColumn}>
+              <i className="fa fa-plus" style={{ paddingRight: "3px" }} />
+              Add another list
+            </Col>
+          </Row>
+        )}
+        {openNewColumn && (
+          <Col className="enter-new-column">
+            <Form.Control
+              size="sm"
+              type="text"
+              placeholder="Enter column title ..."
+              className="input-enter-new-column"
+              ref={newColumnInputRef}
+              value={newColumnTitle}
+              onChange={onNewColumnTitleChange}
+              onKeyDown={(e) => e.key === "Enter" && addNewColumn()}
+            />
+            <Button variant="success" size="sm" onClick={addNewColumn}>
+              Add column
+            </Button>
+            <span className="cancel-new-column" onClick={toggleNewColumn}>
+              <i className="fa fa-trash icon" />
+            </span>
+          </Col>
+        )}
+      </BootContainer>
     </div>
   );
 }
